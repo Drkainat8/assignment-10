3816f66450cec7f1970b111cfb9b9a82b621ac3a
Drkainat8 <161299000+Drkainat8@users.noreply.github.com>
2024-05-15 23:42:45 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+//assignment no 10 adding comment 
+//sum of two numbers
+//describing numbers in variable
+var num1 = 8;
+var num2 = 6;
+//now adding num1 and num 2 in a variable name result
+var resultofsum = num1 + num2;
+//now printing the result in terminal
+console.log("sum of result is =", resultofsum);
---
+++ main.ts
@@ -0,0 +1,14 @@
+//assignment no 10 adding comment 
+//sum of two numbers
+
+//describing numbers in variable
+let num1  : number = 8;
+let num2 : number = 6;
+
+//now adding num1 and num 2 in a variable name result
+let resultofsum = num1 + num2;
+
+//now printing the result in terminal
+console.log("sum of result is =" , resultofsum)
+
+//result of sum is 14
